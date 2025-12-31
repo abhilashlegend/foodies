af8b6b9d1826d0c3bef142bb05d9dabf6d9616d9
abhilashlegend <abhilash456a@live.com>
2025-12-31 13:55:10 +0530

    Added basic route pages

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+export default function slug() {
+    return (
+        <main>
+            <h1 style={{ color: 'white', textAlign: 'center' }}>
+                Meal Details Page
+            </h1>
+        </main>
+    )
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+export default function Share() {
+    return (
+        <main>
+            <h1 style={{ color: 'white', textAlign: 'center' }}>
+                Share a Meal Page
+            </h1>
+        </main>
+    )
+}
