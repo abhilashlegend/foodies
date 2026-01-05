77e50f212ca96c3e9114a347fbfac874bf50ef42
abhilashlegend <abhilash456a@live.com>
2026-01-05 19:23:37 +0530

    Added meal not found page and check

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="error">
+            <h1>Meal Not Found</h1>
+            <p>Unfortunately, the meal you are looking for does not exist.</p>
+        </main>
+    )
+}
