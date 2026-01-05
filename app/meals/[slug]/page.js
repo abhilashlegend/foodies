0a5a293eadd7f288e3b528a110b95ac0184ad3e0
abhilashlegend <abhilash456a@live.com>
2026-01-05 18:36:20 +0530

    rendering meal details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,29 @@
-export default function slug() {
+import classes from './page.module.css'
+import Image from 'next/image';
+import { getMeal } from '../../../lib/meals';
+
+export default function slug({ params }) {
+    const meal = getMeal(params.slug);
     return (
-        <main>
-            <h1 style={{ color: 'white', textAlign: 'center' }}>
-                Meal Details Page
-            </h1>
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                     <h1>
+                        {meal.title}
+                    </h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creatorEmail}`}>{meal.creatorName}</a>
+                    </p>
+                    <div className={classes.summary}>{meal.summary}</div>
+                </div>
+            </header>
+            <main className={classes.main}>
+              <p className={classes.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions, }}></p>
+            </main>
+        </>
+        
     )
 }
